2f50d98199fbba1457ce1c43b9e2e5cfe89da002
shambhvi29 <ajayanshisharma1@gmail.com>
2024-09-08 19:12:09 +0530

    add identity,bitwise,grade

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ print(9)
 print("this is my",1,"atempt")
 print("hello\n",4)
 print("hello to ",end="*")
+name="Ajayanshi"
+age=12
+behaviour=input("please type the behaviour of Ajayanshi")
+print("my name is" ,name,"and i am ",age,"years old and i behave like this",behaviour)
